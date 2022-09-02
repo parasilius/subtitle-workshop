fc58c249409637421e44ba71c7bc8ba427d051d2
Parsa Hosseini <parsahosseini2001@gmail.com>
2022-09-02 18:43:12 +0430

    Move index.html to static

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/static", express.static('./static'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
 })
 
 app.post("/subtitle-sync", (req, res) => {
