62a48062a1a2f70ffe9421ad45b96ac4e6f646d9
Parsa Hosseini <parsahosseini2001@gmail.com>
2022-09-04 17:28:52 +0430

    Modify static files

--- static/script.js
+++ static/script.js
@@ -4,28 +4,32 @@ function showOptions() {
     let episode_number = document.getElementById("mepisode_number");
     let modifyAllEpisodes = document.getElementById("modifyAllEpisodes");
     let modifyAllEpisodesLabel = document.getElementById("modifyAllEpisodesLabel");
+    let modifyAllSeasonEpisodes = document.getElementById("modifyAllSeasonEpisodes");
+    let modifyAllSeasonEpisodesLabel = document.getElementById("modifyAllSeasonEpisodesLabel");
 
     if (isTV.checked == true) {
         season_number.style.display = "block";
         episode_number.style.display = "block";
         modifyAllEpisodes.style.display = "block";
         modifyAllEpisodesLabel.style.display = "block";
+        modifyAllSeasonEpisodes.style.display = "block";
+        modifyAllSeasonEpisodesLabel.style.display = "block";
     } else {
         season_number.style.display = "none";
         episode_number.style.display = "none";
         modifyAllEpisodes.style.display = "none";
         modifyAllEpisodesLabel.style.display = "none";
+        modifyAllSeasonEpisodes.style.display = "none";
+        modifyAllSeasonEpisodesLabel.style.display = "none";
     }
 }
 
-function disableTVOptions() {
-    let modifyAllEpisodes = document.getElementById("modifyAllEpisodes");
+function disableTVSeasonsAndEpisodes() {
+    document.getElementById("mseason_number").disabled = true;
+    document.getElementById("mepisode_number").disabled = true;
+}
 
-    if (modifyAllEpisodes.checked == true) {
-        document.getElementById("mseason_number").disabled = true;
-        document.getElementById("mepisode_number").disabled = true;
-    } else {
-        document.getElementById("mseason_number").disabled = false;
-        document.getElementById("mepisode_number").disabled = false;
-    }
+function disableTVEpisodes() {
+    document.getElementById("mseason_number").disabled = false;
+    document.getElementById("mepisode_number").disabled = true;
 }
